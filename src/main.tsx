c60007e781117528bf341e6e1f9c492eee848dc6
rolandowebdev <rolandopranata@gmail.com>
2023-10-31 11:31:30 +0700

    chore: setup shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import '@fontsource/plus-jakarta-sans'
+import '@/index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from '@/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
